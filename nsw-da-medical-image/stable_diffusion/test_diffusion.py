4355bcdb34ac259c4a426dfdef5120e58a91ed4b
michael-selasi-dzamesi <msfiagbenu@gmail.com>
2023-09-05 15:48:08 +0200

    Fine tuning U-Net

--- nsw-da-medical-image/stable_diffusion/test_diffusion.py
+++ nsw-da-medical-image/stable_diffusion/test_diffusion.py
@@ -6,7 +6,8 @@ from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler, UNet
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-model_id = "stabilityai/stable-diffusion-2-1"
+#model_id = "stabilityai/stable-diffusion-2-1"
+model_id = os.path.join(__location__,'models/v1.0')
 
 unet = UNet2DConditionModel
 
@@ -15,8 +16,8 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-prompt = "Medical image with optical micropscope of a human embryo at development stage t2"
+prompt = "Medical image with optical microscope of a human embryo at development stage t2"
 for i in range(3):
     image = pipe(prompt).images[0]
-    image.save(os.path.join(__location__,f"embryo{i}.png"))
+    image.save(os.path.join(__location__,'generated_images',f"embryo{i}.png"))
 
