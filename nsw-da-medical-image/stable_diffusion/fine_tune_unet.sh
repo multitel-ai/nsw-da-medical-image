9d33a17c55589a2a6e2c5933a6ba66154b7f6447
michael-selasi-dzamesi <msfiagbenu@gmail.com>
2023-09-05 15:53:08 +0200

    Adding bash script to fine tune U-Net

---
+++ nsw-da-medical-image/stable_diffusion/fine_tune_unet.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Set environment variables
+model_version='v1.1'
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export INSTANCE_DIR="./dataset/AA83-7"
+export CLASS_DIR="path_to_class_images"
+export OUTPUT_DIR="./models/$model_version"
+
+# Create OUTPUT_DIR if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+
+# Run the Python code
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path="$MODEL_NAME"  \
+  --instance_data_dir="$INSTANCE_DIR" \
+  --class_data_dir="$CLASS_DIR" \
+  --output_dir="$OUTPUT_DIR" \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --instance_prompt="Medical image with optical microscope of a human embryo at a certain development stage" \
+  --class_prompt="Microscopic image of a human embryo" \
+  --resolution=512 \
+  --train_batch_size=2 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=200 \
+  --max_train_steps=800 \
+  --enable_xformers_memory_efficient_attention
+
+# Optional: Print a message when the script finishes
+echo "Training script completed."
+
+cp ./wandb/latest-run/files/config.yaml "$OUTPUT_DIR"
