31c8e88e72c587eaf97d8fe5b4ab40aa51f3e70c
maxencewynen <maxencewynen@gmail.com>
2023-09-05 15:04:44 +0200

    edit training loop+wandb inclusion

--- nsw_da_medical_image/classifier/train.py
+++ nsw_da_medical_image/classifier/train.py
@@ -19,19 +19,26 @@ import argparse
 from torch.utils.tensorboard.writer import SummaryWriter
 import os
 from model import build_model
-
+import wandb
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--device', type=str, default='0',
                     help='GPU device', )
 parser.add_argument('--batch_size', type=int, default=16,
-                    help='the batch size')
+                    help='batch size')
+parser.add_argument('--num_epochs', type=int, default=50,
+                    help='Max number of epochs')
+parser.add_argument('--lr', type=float, default=0.001,
+                    help='Initial learning rate')
 parser.add_argument('--data_dir', type=int,
                     help='Data path')
 parser.add_argument('--json_file', type=int, default="data_split.json",
                     help='Json file with train/val split')
 parser.add_argument('--pretrained_weights', type=int, default="pretrained",
                     help='Set pretrained weights')
+parser.add_argument('--wandb_project', type=str, default='classifier', 
+                    help='wandb project name')
+
 
 args = parser.parse_args()
 args_pool = {
@@ -94,61 +101,89 @@ class Train:
 
     def run_train(self,
             num_epochs: int,
+            lr: float,
             weights: str,
             dev: torch.device,
             data_dir: str,
-            logger: SummaryWriter | None = None,
+            wandb_project_name: str = None,
+            wandb_run_name: str = None
         ):
             mdl = build_model(weights)
             mdl = mdl.to(dev)
             loss = nn.CrossEntropyLoss()
-            optim = Adam(mdl.parameters())
-            tr_loader = self.get_dataloder(data_dir,"train")
-            val_loader = self.get_dataloader(data_dir,"valid")
+            optim = Adam(mdl.parameters(), lr=lr)
+            
+			lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, T_max=num_epochs)
 
+			tr_loader = self.get_datalaoder(data_dir,"train")
+            val_loader = self.get_dataloader(data_dir,"valid")
+            
+            wandb.login()
+            wandb.init(project=wandb_project_name, mode="online", name=wandb_run_name, 
+                    entity='trail23-medical-image-diffusion')
             
             for epoch in range(num_epochs):
-                tr_loss_val = 0.0
+                epoch_loss = 0.0
+				step = 0
+				print("-" * 10)
+        		print(f"epoch {epoch + 1}/{num_epochs}")
+				
                 mdl.train()
-                for idx, (img, phase, plane, video, time, phase_prog) in enumerate(tr_loader):
+                
+				for idx, (img, phase, plane, video, time, phase_prog) in enumerate(tr_loader):
+                    step += 1 
                     inp = img.to(dev)
                     lbl= phase.to(dev)
-      
-                optim.zero_grad(set_to_none=True)
+
+                    optim.zero_grad(set_to_none=True)     
     
-                pred = mdl(inp)
-                loss_val = loss(pred, lbl).mean()
-                loss_val.backward()
-                tr_loss_val += loss_val.item()
-                optim.step()
-    
-            tr_loss_val /= len(tr_loader)
-    
-            if logger:
-                logger.add_scalar("loss/tr", tr_loss_val, epoch)
-    
+                    pred = mdl(inp)
+
+                    loss_pred = loss(pred, lbl).mean()
+                    loss_pred.backward()
+                    
+                    epoch_loss += loss_pred.item()
+                    optim.step()
+
+                    if step % 100 == 0:
+                        print(f"{step}/{len(tr_loader)}, train_loss: {loss_pred.item():.4f}")
+
+                epoch_loss /= step
+                epoch_loss_values.append(epoch_loss)
+                print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+                
+                lr_scheduler.step()
+                current_lr = optim.param_groups[0]["lr"]
+
+				wandb.log(
+            		{'Training Loss/Total Loss': epoch_loss, 'Learning rate': current_lr}, 
+            		step=epoch) 
+
     
             mdl.eval()
             with torch.no_grad():
                 acc = 0.0
-                val_loss_val = 0.0
+                epoch_loss_val = 0.0
                 for _, (img, phase, _, _, _, _) in enumerate(val_loader):
                     inp = img.to(dev)
                     lbl = phase.to(dev)
     
                     pred = mdl(inp)
                     acc += (pred.argmax(dim=1) == lbl).float().mean().item()
-                    val_loss_val += loss(pred, lbl).mean().item()
+                    epoch_loss_val += loss(pred, lbl).mean().item()
+                
                 acc /= len(val_loader)
-                val_loss_val /= len(val_loader)
+                
+                epoch_loss_val /= len(val_loader)
     
-            if logger:
-                logger.add_scalar("loss/valid", val_loss_val, epoch)
-                logger.add_scalar("acc/valid", acc, epoch)
-
+            wandb.log(
+                {'Validation/Total Loss': epoch_loss_val,
+                 'Validation/Accuracy': acc},
+                step=epoch)
+                
 if __name__ == "__main__":
     train = Train()
-    train.run_train(50, args.weights_path, args.device, args.data_dir,SummaryWriter())
+    train.run_train(num_epochs=args.num_epochs,lr=args.lr, args.weights_path, args.device, args.data_dir,SummaryWriter())
 
 
 
