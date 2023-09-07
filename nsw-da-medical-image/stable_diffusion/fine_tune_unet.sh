3a9f7a5cb45b938ecb25567ccd89e2b3cc940576
echatzo <ariac@MS-01446.ads.multitel.be>
2023-09-07 11:20:20 +0200

    first adaptation to text encoder fine-tune

--- nsw-da-medical-image/stable_diffusion/fine_tune_unet.sh
+++ nsw-da-medical-image/stable_diffusion/fine_tune_unet.sh
@@ -2,7 +2,7 @@
 
 # Set environment variables
 model_version='v1_1'
-export MODEL_NAME="stabilityai/stable-diffusion-2-1"
+export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
 export INSTANCE_DIR="./dataset/AA83-7"
 export CLASS_DIR="path_to_class_images"
 export OUTPUT_DIR="./models/$model_version"
@@ -10,10 +10,12 @@ export OUTPUT_DIR="./models/$model_version"
 # Create OUTPUT_DIR if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 # Run the Python code
-accelerate launch train_dreambooth.py \
-  --pretrained_model_name_or_path="$MODEL_NAME"  \
-  --instance_data_dir="$INSTANCE_DIR" \
+accelerate launch train_dreambooth_lastBen.py \
+  --train_only_unet \
+  --save_starting_step=500 \
   --class_data_dir="$CLASS_DIR" \
+  --pretrained_model_name_or_path="$MODEL_NAME" \
+  --instance_data_dir="$INSTANCE_DIR" \
   --output_dir="$OUTPUT_DIR" \
   --with_prior_preservation --prior_loss_weight=1.0 \
   --instance_prompt="Medical image with optical microscope of a human embryo at a certain development stage" \
@@ -24,10 +26,10 @@ accelerate launch train_dreambooth.py \
   --learning_rate=5e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --num_class_images=200 \
-  --max_train_steps=800 \
-  --enable_xformers_memory_efficient_attention
+  --max_train_steps=800
 
+
+    
 # Optional: Print a message when the script finishes
 echo "Training script completed."
 
