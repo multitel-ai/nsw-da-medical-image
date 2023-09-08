96d17b366fbe90b991929d96f678df468756bb24
echatzo <ariac@MS-01446.ads.multitel.be>
2023-09-08 16:01:14 +0200

    adding validation images and wandb project name management

--- nsw-da-medical-image/stable_diffusion/fine_tune_unet.sh
+++ nsw-da-medical-image/stable_diffusion/fine_tune_unet.sh
@@ -6,11 +6,13 @@ export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
 export INSTANCE_DIR="./dataset/AA83-7"
 export CLASS_DIR="path_to_class_images"
 export OUTPUT_DIR="./models/$model_version"
+export WANDB_PROJECT_NAME="stable-diffusion-2-1-fine-tune-lastBen_$1"
 
 # Create OUTPUT_DIR if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 # Run the Python code
 accelerate launch train_dreambooth_lastBen.py \
+  --wandb_project="$WANDB_PROJECT_NAME"\
   --train_only_unet \
   --save_starting_step=500 \
   --class_data_dir="$CLASS_DIR" \
@@ -19,14 +21,19 @@ accelerate launch train_dreambooth_lastBen.py \
   --output_dir="$OUTPUT_DIR" \
   --with_prior_preservation --prior_loss_weight=1.0 \
   --instance_prompt="Medical image with optical microscope of a human embryo at a certain development stage" \
-  --class_prompt="Microscopic image of a human embryo" \
+  --class_prompt="A grayscale microscopic image of a human embryo" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
   --learning_rate=5e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=800
+  --max_train_steps=800 \
+  --num_class_images=200 \
+  --num_validation_images=5 \
+  --validation_steps=100\
+  --validation_prompt="Medical image with optical microscope of a human embryo at a certain development stage"
+
 
 
     
