addc9dd015ce3da1c4307bbba32b5e865406d043
maxencewynen <maxencewynen@gmail.com>
2023-09-08 12:30:49 +0200

    weights to device

--- nsw_da_medical_image/classifier/train.py
+++ nsw_da_medical_image/classifier/train.py
@@ -82,7 +82,7 @@ def run_train(num_epochs: int,
     dev = get_device()
     mdl = build_model(path=weights)
     mdl = mdl.to(dev)
-    loss = nn.CrossEntropyLoss(weight=get_weights(data_dir,args.json_file))
+    loss = nn.CrossEntropyLoss(weight=get_weights(data_dir,args.json_file).to(dev))
     optim = Adam(mdl.parameters(), lr=lr)
     
     now = datetime.now()
