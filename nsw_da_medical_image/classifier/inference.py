29b5467903c9e98d77547e6d56b73c5fb554d887
maxencewynen <maxencewynen@gmail.com>
2023-09-08 09:54:55 +0200

    argparse inference + add split as argument

--- nsw_da_medical_image/classifier/inference.py
+++ nsw_da_medical_image/classifier/inference.py
@@ -6,15 +6,16 @@ from torch.utils.data.dataloader import DataLoader
 from nsw_da_medical_image.classifier.utils import get_dataloader
 import pathlib
 from tqdm import tqdm
-
+import argparse
 
 def inference(
         data_path: str,
         out_prob_path: str,
         out_class_path: str,
+        split: str = "val",
         model_path: str | None = None,
-        batch_size: int = 8,
-        json_file: str = "/App/data/split.json") -> None:
+        batch_size: int = 32,
+        json_file: str = "/App/code/split.json") -> None:
     """
     Runs a trained model on data and outputs two csv files: one containing the probabilities and another containing the predicted classes.
 
@@ -37,7 +38,7 @@ def inference(
     data_path = pathlib.Path(data_path)
 
     dataloader = get_dataloader(data_dir=data_path,
-                                mode='test_set',
+                                mode=val,
                                 batch_size=batch_size,
                                 json_file=json_file)
     
@@ -68,11 +69,22 @@ def inference(
     prob_df.to_csv(out_prob_path, index=False)
     class_df.to_csv(out_class_path, index=False)
 
+def main(args):
+    inference(args.data_path, args.out_prob_path, args.out_class_path, args.split,
+              args.model_path, args.batch_size, args.json_file)
 
 
-if __name__ =="__main__":
-    # example 
-    inference(data_path="/App/data/extracted", 
-            out_class_path="/App/data/results/inference.csv",
-            out_prob_path="/App/data/results/inference_probs.csv",
-            batch_size=64)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run inference on a model and output results to CSV files.",
+					formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("data_path", type=str, help="Path to the data.")
+    parser.add_argument("out_prob_path", type=str, help="Path to the CSV file to be created as output for probabilities.")
+    parser.add_argument("out_class_path", type=str, help="Path to the CSV file to be created as output for classes.")
+    parser.add_argument("--split", type=str, default="val", help="Which data split (test, val). Default is 'val'.")
+    parser.add_argument("--model_path", type=str, default=None, help="Path to the trained model weights. Default is None.")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for the dataloader. Default is 8.")
+    parser.add_argument("--json_file", type=str, default="/App/code/split.json", help="Path to the JSON file that contains data splits. Default is '/App/data/split.json'.")
+
+    args = parser.parse_args()
+    main(args)
