2a6540e91664e1e1bde33d3b7b63656a3868a306
maxencewynen <maxencewynen@gmail.com>
2023-09-08 15:54:53 +0200

    refactor inference

--- nsw_da_medical_image/classifier/inference.py
+++ nsw_da_medical_image/classifier/inference.py
@@ -1,30 +1,38 @@
-import torch
-import pandas as pd
-from nsw_da_medical_image.classifier.model import build_model
-import nsw_da_medical_image.dataset_util as du
-from torch.utils.data.dataloader import DataLoader
-from nsw_da_medical_image.classifier.utils import get_dataloader
+import os
+import argparse
 import pathlib
 from tqdm import tqdm
-import argparse
+import pandas as pd
+import torch
+from torch.utils.data.dataloader import DataLoader
+from nsw_da_medical_image.classifier.model import build_model
+import nsw_da_medical_image.dataset_util as du
+from nsw_da_medical_image.classifier.utils import get_dataloader
 
 def inference(
         data_path: str,
-        out_prob_path: str,
-        out_class_path: str,
+        out_path: str,
         split: str = "val",
         model_path: str | None = None,
         batch_size: int = 32,
-        json_file: str = "/App/code/split.json") -> None:
+        json_file: str = "/App/code/split.json"
+    ) -> None:
     """
     Runs a trained model on data and outputs two csv files: one containing the probabilities and another containing the predicted classes.
 
     Args:
     data_path: path to the data
-    out_prob_path: path to the csv file to be created as output for probabilities
-    out_class_path: path to the csv file to be created as output for classes
+    out_path: path to the output directory
+    split: Data split, either 'test' or 'val'
     model_path: path to the trained model weights
+    batch_size: Batch size for the dataloader
+    json_file: Path to the JSON file that contains data splits
     """
+    data_path = pathlib.Path(data_path)
+    out_path = pathlib.Path(out_path)
+    model_path = pathlib.Path(model_path)
+    model_name = model_path.stem
+    
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = build_model(net='densenet121',path=model_path).to(device)
     model.eval()
@@ -34,7 +42,7 @@ def inference(
     prob_preds = []
     class_preds = []
 
-    data_path = pathlib.Path(data_path)
+    os.makedirs(out_path, exist_ok=True)
 
     dataloader = get_dataloader(data_dir=data_path,
                                 mode=split,
@@ -62,29 +70,37 @@ def inference(
 
         torch.cuda.empty_cache()
         del img, batch_predictions
+        break
 
     prob_df = pd.concat([prob_df, pd.DataFrame(prob_preds)], ignore_index=True)
     class_df = pd.concat([class_df, pd.DataFrame(class_preds)], ignore_index=True)
 
-    prob_df.to_csv(out_prob_path, index=False)
-    class_df.to_csv(out_class_path, index=False)
+    prob_df.to_csv(out_path / (model_name + "_pred-prob.csv"), index=False)
+    class_df.to_csv(out_path / (model_name + "_pred-class.csv"), index=False)
 
-def main(args):
-    inference(args.data_path, args.out_prob_path, args.out_class_path, args.split,
-              args.model_path, args.batch_size, args.json_file)
+def main(args: argparse.Namespace) -> None:
+    inference(
+        args.data_path,
+        args.out_path,
+        args.split,
+        args.model_path,
+        args.batch_size,
+        args.json_file
+    )
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Run inference on a model and output results to CSV files.",
-					formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description="Run inference on a model and output results to CSV files.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
-    parser.add_argument("data_path", type=str, help="Path to the data.")
-    parser.add_argument("out_prob_path", type=str, help="Path to the CSV file to be created as output for probabilities.")
-    parser.add_argument("out_class_path", type=str, help="Path to the CSV file to be created as output for classes.")
-    parser.add_argument("--split", type=str, default="val", help="Which data split (test, val). Default is 'val'.")
-    parser.add_argument("--model_path", type=str, default=None, help="Path to the trained model weights. Default is None.")
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for the dataloader. Default is 8.")
-    parser.add_argument("--json_file", type=str, default="/App/code/split.json", help="Path to the JSON file that contains data splits. Default is '/App/data/split.json'.")
+    parser.add_argument("--data_path", type=str, help="Path to the data.")
+    parser.add_argument("--out_path", type=str, help="Path to output directory.")
+    parser.add_argument("--model_path", type=str, help="Path to the trained model weights.")
+    parser.add_argument("--split", type=str, default="val", help="Which data split (test, val).")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for the dataloader.")
+    parser.add_argument("--json_file", type=str, default="/App/code/split.json", help="Path to the JSON file that contains data splits.")
 
     args = parser.parse_args()
     main(args)
