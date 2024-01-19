1af185e52086e59d52b19731dc276fd534e8d824
Maxime Amodei <maxime.amodei@uliege.be>
2024-01-19 09:52:30 +0100

    feat: allow phase selection & median selection

--- nsw_da_medical_image/dataset_util/__init__.py
+++ nsw_da_medical_image/dataset_util/__init__.py
@@ -1,2 +1,4 @@
-from .dataset import NSWDataset, VideoPhases
+from .dataset import NSWDataset
 from .enums import Video, Phase, FocalPlane
+
+__all__ = ["NSWDataset", "Video", "Phase", "FocalPlane"]
